6739efdef539e9fd58af917364429e80dac73dd5
jlam303 <jplam904@gmail.com>
2022-11-16 18:05:04 -0700

    Finished functionality

---
+++ ca.js
@@ -0,0 +1,28 @@
+function insertNum(num)
+{
+    document.getElementById("inputScreen").value +=num;
+}
+function addNum()
+{
+    document.getElementById("inputScreen").value +="+";
+}
+function subtractNum()
+{
+    document.getElementById("inputScreen").value +="-";
+}
+function divideNum()
+{
+    document.getElementById("inputScreen").value +="/";
+}
+function multiplyNum()
+{
+    document.getElementById("inputScreen").value +="*";
+}
+function clearNum()
+{
+    document.getElementById("inputScreen").value ="";
+}
+function calcNum()
+{
+    document.getElementById("inputScreen").value = eval(document.getElementById("inputScreen").value)
+}
